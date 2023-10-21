1d0a47f5d6f6134926e5a34772deda596948578d
BarisYildirim00 <b.yildirimbladeco@gmail.com>
2023-10-21 21:11:37 +0300

    User API Added

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,32 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 dotenv.config();
 import connectDB from "./config/db.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
 const port = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
 
+//body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//cokkie parser middleware
+app.use(cookieParser());
+
 app.get("/", (req, res) => {
   res.send("API is Runing...");
 });
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
+
 app.use(notFound);
 app.use(errorHandler);
 
