fbc31dc1025125e6425b2e404cfb8f04745c2872
BarisYildirim00 <b.yildirimbladeco@gmail.com>
2023-10-21 15:41:38 +0300

    cartScreen Added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -14,6 +14,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+import CartScreen from "./screens/CartScreen.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -24,6 +25,7 @@ const router = createBrowserRouter(
         path="/product/:id"
         element={<ProductScreen />}
       ></Route>
+      <Route index={false} path="/cart" element={<CartScreen />}></Route>
     </Route>
   )
 );
